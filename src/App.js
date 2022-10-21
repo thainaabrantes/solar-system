103f546a5e161f28237851e865657006d385ef91
Thaina-Abrantes <oliveiraabrantess@gmail.com>
2022-10-21 17:23:50 -0300

    Cria componente SolarSystem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends Component {
   render() {
-    return (<Header />);
+    return (
+      <div>
+        <Header />
+        <SolarSystem />
+      </div>
+    );
   }
 }
 
